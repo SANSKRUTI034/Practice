1c5239530132ccb0c18faa9400e8f6601817043d
Sanskruti Pande <161758224+SANSKRUTI034@users.noreply.github.com>
2026-04-14 23:58:29 +0530

    Add files via upload
    Take 1 string as a input and reverse the string

---
+++ program260.c
@@ -0,0 +1,46 @@
+//take 1 string as a input and reverse the string
+
+#include<stdio.h>
+
+void StrRevX (char *str)
+{
+    char  *start = str ;
+    char *end = str;
+    char temp = '\0';
+
+
+    while(*end != '\0')
+    {
+        end++;
+    }
+    end--;
+
+    while(start < end)
+    {
+        temp = *start ;
+        *start = *end ;
+        *end = temp;
+
+        start ++;
+        end--;
+    }
+    
+
+}
+
+
+int main()
+{
+    char Arr[50] = {'\0'};
+
+    printf("Enter string: \n");
+    scanf("%[^'\n']s", Arr);
+
+
+
+    StrRevX(Arr);
+    printf("Updated string is : %s\n",Arr);
+
+
+    return 0;
+}
